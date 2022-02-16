ebf1acad911302c2fd23c3bf1a7048621fb5aecc
ali484 <micahelwwww@gmail.com>
2022-02-16 13:00:37 +0330

    added migration files

---
+++ database/migrations/2022_02_14_201900_create_guests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->string('fullname');
+            $table->smallInteger('age');
+            $table->boolean('gender');
+            $table->char('ssn',10);
+            $table->char('phone',11);
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+};
